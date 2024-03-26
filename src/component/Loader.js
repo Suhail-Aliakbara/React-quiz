8c828960c5d7273c45b44879abc58b447ba187a1
Suhail-Aliakbara <suhailaliakbara99@gmail.com>
2024-03-26 20:01:38 +0530

    added answer selection and Next button

--- src/component/Loader.js
+++ src/component/Loader.js
@@ -1,8 +1,9 @@
+import React from "react";
+
 export default function Loader() {
   return (
     <div className="loader-container">
-      <div className="loader"></div>
-      <p>Loading questions...</p>
+      <div className="loader">Loading questions...</div>
     </div>
   );
 }
